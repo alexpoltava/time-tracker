d9fa1bd544de79dee45f4d030c6926d6df00c616
Alex Omelchenko <a.n.omelchenko@icloud.com>
2017-08-09 00:30:41 +0300

    basic total added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import duration from './utils/duration';
+
 import Menu from './Menu.jsx';
 import style from './Sidebar.less';
 
+const mapStateToProps = state => ({
+    timers: state.timers,
+});
+
+@connect(mapStateToProps)
 export default class Sidebar extends Component {
     render() {
         return (
             <div className={style.root}>
-                <h2>Total:</h2>
-                <h2>00:00</h2>
+                <h2>Total logged:</h2>
+                <h2>{duration(this.props.timers.reduce((prev, curr) => prev + curr.time, 0))}</h2>
+                <span>with {this.props.timers.length} tasks</span>
                 <Menu onSelectMenuItem={this.props.onSelectMenuItem} />
             </div>
         );
